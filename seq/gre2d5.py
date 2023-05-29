5c580d27a67cae0ff89c34b7585d94224509802a
Feiyang Lou <103250574+FeiyangLou@users.noreply.github.com>
2023-05-29 10:18:55 +0800

    Update gre2d5.py
    fix RO predephase calculation error

--- seq/gre2d5.py
+++ seq/gre2d5.py
@@ -68,7 +68,7 @@ class GRE2D5(blankSeq.MRIBLANKSEQ):
             return 0
         
         # 计算RO predephase梯度大小
-        ROpreAmp = -self.readAmp * (self.readoutTime + self.riseTime) / (self.phaseTime + self.riseTime)
+        ROpreAmp = -0.5 * self.readAmp * (self.readoutTime + self.riseTime) / (self.phaseTime + self.riseTime)
 
         self.phaseAmpMax = self.phaseAmp * (self.nPoints - 1) / 2
         if self.phaseAmpMax > 1:
@@ -130,9 +130,11 @@ class GRE2D5(blankSeq.MRIBLANKSEQ):
                 tim += self.riseTime + self.readPadding
                 self.rxGateSync(tim, acq_time)
 
-                # spoil
+                # slice spoil
                 tim += acq_time - self.readPadding + self.riseTime + self.spoilDelay
                 gradient(tim, self.phaseTime, self.phaseAmpMax*uniform(-1, 1), self.axes[2])
+                # phase rewind
+                gradient(tim, self.phaseTime, -(self.nPoints/2 - j) * self.phaseAmp, self.axes[1])
 
         self.endSequence(self.nPoints * self.nScans * self.t_r + 2e6)
         # --------------------- ↑序列结束↑ ---------------------
